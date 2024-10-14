b0f31f825c228c9a9c933384ce34b9acd3b7f3a5
Tsavorite <nezax.sj@gmail.com>
2024-10-14 21:28:32 +0200

    header+home-section1

--- constants/header.php
+++ constants/header.php
@@ -1,16 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
-    <link rel="stylesheet" href="./includes/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="./includes/css/theme.css">
     <link rel="stylesheet" href="./includes/css/style.css">
 </head>
 
-<body>
-    <header>
-        <h5>this will be header</h5>
-    </header>
+<body class="bg-dark">
+    <nav class="navbar navbar-expand-lg bg-dark sticky-top">
+        <div class="container-fluid">
+            <div>
+                <a class="navbar-brand" href="#">LOGO</a>
+            </div>
+            <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Stop Motion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Hand-Drawn</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Computer 3D</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Modern 2D</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <button class="btn btn-accent bi bi-person-circle" type="button"></button>
+                <button class="btn btn-primary ms-2" type="button">Log in</button>
+            </div>
+        </div>
+    </nav>
+
+    </nav>
--- views/index.php
+++ views/index.php
@@ -3,6 +3,34 @@ $page_title = "Front Page";
 include_once "./constants/header.php";
 ?>
 
+<div class="d-flex">
+    <img class="banner-img" src="./includes/assets/tnbc.jpg" alt="">
+    <img class="banner-img" src="./includes/assets/treasure-planet.jpg" alt="">
+    <img class="banner-img" src="./includes/assets/shrek.jpg" alt="">
+    <img class="banner-img" src="./includes/assets/twnotw.jpg" alt="">
+</div>
+
+<nav class="navbar navbar-expand-lg bg-transparent sticky-top justify-content-center">
+    <div class="bg-dark">
+        <div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Stop Motion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hand-Drawn</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Computer 3D</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Modern 2D</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
         Dropdown button
